b7cb8fbf8e5ff797ebd4b687d1709a2aa8dd7c54
Hector Lozano <hlozano2@cnm.edu>
2016-10-05 14:35:48 -0600

    Added Test Page to ensure PhpStorm is set up

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>The Vitamin Store</title>
+	</head>
+	<body>
+		<table summary="Home Page">
+			<tr>
+				<td width="20%"><a href="index.html">Home</a> </td>
+				<td width="20%"><a href="buy.html">Buy Now</a> </td>
+				<td width="20%"><a href="products.html">Products and Services</a> </td>
+				<td width="20%"><a href="yourhealth.html"></a> </td>
+				<td width="20%"><a href="about.html">About Us</a> </td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					 <h3>Welcome to the Vitamin Store: Protecting Your Health Naturally!</h3>
+				<p>Founded April 30, 2016 The Vitamin Store is veteran owned and operated.
+				We offer the best herbal medication that money can buy</p>
+				<p>Many Companies that talk about Customer Service fail to deliver.
+				We've Served Our Country, Now We Are Redy To Serve You!</p>
+				</td>
+				</tr>
+			<tr>
+				<td colspan="3">The Vitamin Store &copy; 2016 NinjaMedic LLC</td>
+				<td><a href="contact.html">Contact Us</a> </td>
+				<td><a href="sitemap.html">Site Map</a> </td>
+			</tr>
+		</table>
+	</body>
+</html>
